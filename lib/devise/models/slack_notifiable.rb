943c780de69cfe0bcdf2ea8d44b220642b962612
whitemerry <whitemerry@outlook.com>
2020-06-21 23:02:15 +0200

    Rdoc's

--- lib/devise/models/slack_notifiable.rb
+++ lib/devise/models/slack_notifiable.rb
@@ -2,20 +2,26 @@
 
 module Devise
   module Models
+    # Slack notifications for devise models!
     module SlackNotifiable
       extend ActiveSupport::Concern
 
       included do
+        # Rails hook
+        # Triggers slack registration message
         after_create :registration_notification
 
         protected
 
+        # Devise hook
+        # Triggers slack confirmation message
         def after_confirmation
           confirmation_notification
 
           super if defined?(super)
         end
 
+        # Sends slack notification if confirmation_message_enabled
         def confirmation_notification
           return unless DeviseSlackNotifiable.configuration.confirmation_message_enabled
 
@@ -24,6 +30,7 @@ module Devise
           )
         end
 
+        # Sends slack notification
         def registration_notification
           send_slack_notification(
             DeviseSlackNotifiable.configuration.registration_message_formatter
@@ -32,6 +39,9 @@ module Devise
 
         private
 
+        # Creates notifier and sends message
+        #
+        # @param [Proc] formatter
         def send_slack_notification(formatter)
           notifier = DeviseSlackNotifiable::Notifier.new
           notifier.send_message(self, formatter)
