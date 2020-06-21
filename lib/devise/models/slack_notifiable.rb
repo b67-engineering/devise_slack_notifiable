0b9f8cf1c85e7a1ca431f9a1d3426d54b59aedd6
whitemerry <whitemerry@outlook.com>
2020-06-21 16:36:27 +0200

    Create hooks

--- lib/devise/models/slack_notifiable.rb
+++ lib/devise/models/slack_notifiable.rb
@@ -3,7 +3,27 @@
 module Devise
   module Models
     module SlackNotifiable
+      extend ActiveSupport::Concern
 
+      included do
+        after_create :registration_notification
+
+        protected
+
+        def after_confirmation
+          confirmation_notification
+
+          super
+        end
+
+        def confirmation_notification
+          # FIXME: Create notification after confirmation
+        end
+
+        def registration_notification
+          # FIXME: Create notification after registration
+        end
+      end
     end
   end
 end
--- lib/devise_slack_notifiable.rb
+++ lib/devise_slack_notifiable.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'devise_slack_notifiable/version'
-require 'devise/model/slack_notifable'
+require 'devise/models/slack_notifiable'
 
 module DeviseSlackNotifiable
   class Error < StandardError; end
